720307360db1158042e939b0ed13df987a372df8
Tyler Lentz <tlentz@ucsd.edu>
2022-05-02 18:47:17 -0700

    Updated firebase config

--- js/analytics.js
+++ js/analytics.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-analytics.js";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyCMNh_-P8eezJ3rfD6BC28qlHkggAp0ehk",
-authDomain: "cse134b-hw5-1149b.firebaseapp.com",
-projectId: "cse134b-hw5-1149b",
-storageBucket: "cse134b-hw5-1149b.appspot.com",
-messagingSenderId: "132890213985",
-appId: "1:132890213985:web:5963907d7cb10958c9b420",
-measurementId: "G-E8J3RXS2T3"
+apiKey: "AIzaSyAtb7DzOShvBtuHq1z_dxuIc4oDsBWzaIo",
+authDomain: "tyler-lentz.firebaseapp.com",
+projectId: "tyler-lentz",
+storageBucket: "tyler-lentz.appspot.com",
+messagingSenderId: "639760361419",
+appId: "1:639760361419:web:81086db94f44ef9467b84e",
+measurementId: "G-1C7HDBWNZN"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
